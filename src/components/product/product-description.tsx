b55570481b342a291f2e006e94989a0c9c986a86
Fabinho Flauzino <FabioFlauzino@hotmail.com>
2025-09-22 20:12:45 -0300

    Criar Descrição do produto

--- src/components/product/product-description.tsx
+++ src/components/product/product-description.tsx
@@ -1,11 +1,37 @@
 "use client"
 
+import Image from "next/image"
+import { useState } from "react"
+
 type Props = {
   text: string
 }
 
 export function ProductDescription({ text }: Props) {
+  const [opened, setOpened] = useState(true)
+
   return (
-    <div className="">...</div>
+    <div className="bg-white border border-gray-200 px-7 mt-20">
+      <div className={`flex items-center justify-between py-7 ${opened ? "border-b" : "border-0"} border-gray-200`}>
+        <div className="text-2xl font-medium">Informações do produto</div>
+        <div
+          onClick={() => setOpened(!opened)}
+          className={`size-14 border border-gray-200 flex justify-center items-center rounded-sm`}>
+          <Image
+            src={'/assets/ui/arrow-left-s-line.png'}
+            alt=""
+            width={24}
+            height={24}
+            className={`inline-block cursor-pointer ${opened ? 'rotate-0' : 'rotate-180'} transition-all`}
+          />
+        </div>
+
+      </div>
+      {opened && (
+        <div className={`text-gray-500 my-12`}>
+          {text}
+        </div>
+      )}
+    </div>
   )
 }
